5764fadbe2aa9aa3f13aa24996a8ed077f42bd26
Amujo Dotun <samue;amujo@gmail.com>
2019-09-04 22:12:33 +0100

    student and admin login and logout logic done

---
+++ Admin/hall.php
@@ -0,0 +1,28 @@
+<?php include('../core/server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+	
+  <form method="post" action="hall.php">
+  	<?php include('../core/errors.php'); ?>
+  	<div class="input-group">
+  	  <label>HallName</label>
+  	  <input type="text" name="hallname" value="<?php echo $hallname; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>HallCapacity</label>
+  	  <input type="number" name="hallcapacity" value="<?php echo $hallcapacity; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_hall">Register</button>
+  	</div>
+  </form>
+</body>
+</html>
---
+++ core/server.php
@@ -0,0 +1,254 @@
+<?php
+session_start();
+
+// Project Title 
+
+$title = 'SeatAllocation System';
+
+// initializing variables
+$username = "";
+$email    = "";
+$hallname ="";
+$hallcapacity ="";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'examseatallocation');
+
+
+
+// REGISTER ADMIN
+if (isset($_POST['reg_admin'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM admins WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO students (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: ../Admin/index.php');
+  }
+}
+
+
+
+// LOGIN ADMIN
+if (isset($_POST['login_admin'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM students WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: ../Admin/index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+//...end of login logic
+
+
+
+
+
+
+
+// REGISTER STUDENT
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM students WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO students (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+
+// ...
+
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM students WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+//...end of login logic
+
+
+
+
+
+// REGISTRATION OF HALL
+if (isset($_POST['reg_hall'])) {
+  // receive all input values from the form
+  $hallname = mysqli_real_escape_string($db, $_POST['hallname']);
+  $hallcapacity = mysqli_real_escape_string($db, $_POST['hallcapacity']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($hallname)) { array_push($errors, "Hallname is required"); }
+  if (empty($hallcapacity)) { array_push($errors, "hallcapacity is required"); }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM hall WHERE hallname='$hallname' OR hallcapacity='$hallcapacity' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['hallname'] === $hallname) {
+      array_push($errors, "Hallname already exists");
+    }
+  }
+
+  // Finally, register hall if there are no errors in the form
+  if (count($errors) == 0) {
+
+  	$query = "INSERT INTO hall (hallname, hallcapacity) 
+  			  VALUES('$hallname', '$hallcapacity')";
+  	mysqli_query($db, $query);
+  	$_SESSION['hallname'] = $hallname;
+  	$_SESSION['success'] = "You have successfully register hall";
+  	header('location: ../students/index.php');
+  }
+}
+
+
+
+
+// REGISTRATION OF DEPARTMENT
+if (isset($_POST['reg_dept'])) {
+  // receive all input values from the form
+  $department = mysqli_real_escape_string($db, $_POST['department']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($department)) { array_push($errors, "Department name is required"); }
+
+  // first check the database to make sure 
+  // a department does not already exist with the same department name
+  $user_check_query = "SELECT * FROM departments WHERE department='$department' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if department exists
+    if ($user['department'] === $department) {
+      array_push($errors, "Department already exists");
+    }
+  }
+
+  // Finally, register hall if there are no errors in the form
+  if (count($errors) == 0) {
+
+      $query = "INSERT INTO department (department) 
+                VALUES('$department')";
+      mysqli_query($db, $query);
+      $_SESSION['department'] = $department;
+      $_SESSION['success'] = "You have successfully register department name";
+      header('location: ../students/index.php');
+  }
+}
+
+
+
+?>
