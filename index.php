c8ef76ed78c4ecd7dd430b02f8888f21d7c0fa58
AmujoDotun <samuelamujo@gmail.com>
2019-12-04 15:58:17 +0100

    Added and updated the home page

--- index.php
+++ index.php
@@ -1,90 +1,67 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title> Welcome to Exam Seating Arrangement</title>
-  <link rel="stylesheet" href="css/bootstrap.min.css"/>
-  <link rel="stylesheet" href="css/bootstrap.css"/>
-  <link rel="stylesheet" href="css/my.css"/>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <script src="js/jquery.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-</head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Exam-Seat-Allocation</title>
+  </head>
 <body>
-  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-<div class="container">
-           <!-- Brand and toggle get grouped for better mobile display -->
-           <div class="navbar-header">
-               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                   <span class="sr-only">Toggle navigation</span>
-                   <span class="icon-bar"></span>
-                   <span class="icon-bar"></span>
-                   <span class="icon-bar"></span>
-               </button>
-               <a class="navbar-brand" href="#">Exam Seating Arrangement</a>
-           </div>
-           <!-- Collect the nav links, forms, and other content for toggling -->
-           <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
-               <ul class="nav navbar-nav">
-                   <li class="active">
-                       <a href="firstpage.php" class="active">Home</a>
-                   </li>
-                   <li>
-                       <a href="adminloginde.php">Admin</a>
-                   </li>
-                   <li>
-                       <a href="contactus.php">Contact us</a>
-                   </li>
-               </ul>
-           </div>
-           <!-- /.navbar-collapse -->
-       </div>
-       <!-- /.container -->
-   </nav>
-  <div class="carousel fade-carousel slide" data-ride="carousel" data-interval="2500" id="bs-carousel">
-  <!-- Overlay -->
-  <div class="overlay"></div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Exam-Seat-Allocation</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
-    <li data-target="#bs-carousel" data-slide-to="1"></li>
-    <li data-target="#bs-carousel" data-slide-to="2"></li>
-  </ol>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Admin/adminLogin.php">Admin</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="students/login.php">Student<span class="sr-only">(current)</span></a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 
-  <!-- Wrapper for slides -->
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="item slides active">
-      <div class="slide-1"></div>
-      <div class="screen-word">
-        <hgroup>
-            <h2></h2>
-        </hgroup>
-      <a href="selectoneof3.php" class="btn btn-hero btn-lg btn-responsive">SEARCH YOU</a>
-      </div>
+    <div class="carousel-item active">
+        <img src="images/classroom2.jpg" class="d-block w-100" alt="...">
     </div>
-    <div class="item slides">
-      <div class="slide-2"></div>
-      <div class="screen-word">
-        <hgroup>
-            <h2></h2>
-        </hgroup>
-      <a href="selectoneof3.php" class="btn btn-hero btn-lg btn-responsive">SEARCH YOU</a>
-      </div>
+    <div class="carousel-item">
+      <img src="images/classroom6.jpg" class="d-block w-100" alt="...">
     </div>
-    <div class="item slides">
-      <div class="slide-3"></div>
-      <div class="screen-word">
-        <hgroup>
-          <h2></h2>
-        </hgroup>
-      <a href="selectoneof3.php" class="btn btn-hero btn-lg btn-primary btn-responsive">SEARCH YOU</a>
-      </div>
+    <div class="carousel-item">
+      <img src="images/classroom4.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/classroom5.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/classroom3.png" class="d-block w-100" alt="...">
     </div>
   </div>
 </div>
 
-</body>
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
 </html>
