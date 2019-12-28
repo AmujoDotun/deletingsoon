ae25a6d4d12e6515eb2d1690eefb1287a5f72798
AmujoDotun <samuelamujo@gmail.com>
2019-12-28 02:24:19 +0100

    table for seat allocation

--- Admin/timetable.php
+++ Admin/timetable.php
@@ -1,6 +1,11 @@
 <?php 
 include  ('../core/server.php'); 
 include ('../core/header.php');
+
+$sql = "SELECT * FROM trisub ORDER BY department";
+$squery = $db->query($sql);
+// $sql = "SELECT  FROM MyGuests";
+// $result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,40 +17,32 @@ include ('../core/header.php');
 </head>
 <body>
  <?php 
-$username ='dotun';
-$matric ="2019899299222";
-$levels ="ND1";
-$year ="2019";
+// $username ='dotun';
+// $matric ="2019899299222";
+// $levels ="ND1";
+// $year ="2019";\
+
 
  ?>
 <table class="table table-dark">
+
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
+      <th scope="col">First\</th>
       <th scope="col">Last</th>
       <th scope="col">Handle</th>
     </tr>
   </thead>
   <tbody>
+  <?php while(mysqli_fetch_assoc($squery)) :?>
     <tr>
       <th scope="row">1</th>
-      <td><?php echo $username; ?></td>
-      <td><?php echo $year; ?></td>
-      <td><?php echo $matric; ?></td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
       <td>Jacob</td>
       <td>Thornton</td>
       <td>@fat</td>
     </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    <?php endwhile; ?>
   </tbody>
 </table>
 </body>
