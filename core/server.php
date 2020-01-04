ae7c07f2f66b8d8a2a82372e2b12f17829050c8f
AmujoDotun <samuelamujo@gmail.com>
2020-01-04 23:47:38 +0100

    done with the seatno reg

--- core/server.php
+++ core/server.php
@@ -297,16 +297,17 @@ if (isset($_POST['reg_seat'])) {
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM trisub WHERE level='$level' OR department='$department' LIMIT 1";
-  $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
+
+  // $user_check_query = "SELECT * FROM trisub WHERE level='$level' OR department='$department' LIMIT 1";
+  // $result = mysqli_query($db, $user_check_query);
+  // $user = mysqli_fetch_assoc($result);
   
   
 
   // Finally, register hall if there are no errors in the form
   if (count($errors) == 0) {
 
-  	$query = "INSERT INTO trisub (level, department, hallname, examdate, starttime, endtime) 
+  	$query = "INSERT INTO seatno (level, department, hallname, examdate, starttime, endtime) 
         VALUES('$level', '$department', '$hallname', '$examdate', '$starttime', '$endtime')";
   	mysqli_query($db, $query);
   	$_SESSION['level'] = $level;
@@ -326,13 +327,13 @@ function allocate($department){
 
     while ($array = mysqli_fetch_array($result)) {
     // sql query to check if student has been allocated
-    $check = "SELECT * FROM `trisub` WHERE student_id = {$array['id']}";
+    $check = "SELECT * FROM `seatno` WHERE student_id = {$array['id']}";
     $checkresult = $db->query($check);
       if(mysqli_num_rows($checkresult)> 0){
        
       }
       else{
-        $sql = "INSERT INTO  trisub() 
+        $sql = "INSERT INTO  seatno() 
               VALUES (NULL,{$array['id']}, '$department', $counter, NULL);";
         $counter++;
       }
@@ -364,43 +365,9 @@ function allocate($department){
 //call function
 // echo allocate("computer science", "computer engineering", "accountacy");
 
-echo allocate("computer");
+// echo allocate("computer");
 
 
-// if (isset($_POST['reg_seat'])) {
-//   // receive all input values from the form
-//   $levels = mysqli_real_escape_string($db, $_POST['levels']);
-//   $department = mysqli_real_escape_string($db, $_POST['department']);
-//   $enrollnumber = mysqli_real_escape_string($db, $_POST['enrollnumber']);
-//   $classroom = mysqli_real_escape_string($db, $_POST['classroom']);
-//   $course = mysqli_real_escape_string($db, $_POST['course']);
-//   $examdate = mysqli_real_escape_string($db, $_POST['examdate']);
-//   $starttime = mysqli_real_escape_string($db, $_POST['starttime']);
-//   $endtime = mysqli_real_escape_string($db, $_POST['endtime']);
-
-//   // form validation: ensure that the form is correctly filled ...
-//   // by adding (array_push()) corresponding error unto $errors array
-  // if (empty($levels)) { array_push($errors, "Levels is required"); }
-  // if (empty($department)) { array_push($errors, "department is required"); }
-  // if (empty($enrollnumber)) { array_push($errors, "Enrollnumber is required"); }
-  // if (empty($classroom)) { array_push($errors, "Classroom is required"); }
-  // if (empty($course)) { array_push($errors, "Course is required"); }
-  // if (empty($examdate)) { array_push($errors, "Examdate is required"); }
-  // if (empty($starttime)) { array_push($errors, "Start Time is required"); }
-  // if (empty($endtime)) { array_push($errors, "EndTime is required"); }
-
-
-  
-
-//   // Finally, register user if there are no errors in the form
-//   if (count($errors) == 0) {
-  
-//   	$query = "INSERT INTO seatno (levels, department, enrollnumber, classroom, subjects, examdate, starttime, endtime) 
-//   			  VALUES('$levels', $department', '$enrollnumber', '$classroom', '$course','$examdate', '$starttime', '$endtime')";
-//   	mysqli_query($db, $query);
-//   	echo "you have succefully assign seat for the sudent of " .$department;
-//   	// header('location: ../Admin/index.php');
-//   }
 // }
 
 
